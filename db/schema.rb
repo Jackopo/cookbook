cde1f9e04b0c841329f17833fc710962a7cb627e
jackopo <jacopo.ghisolfi@infopark.de>
2015-04-07 22:19:46 +0200

    Added associations between importante Models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406152726) do
+ActiveRecord::Schema.define(version: 20150407201850) do
+
+  create_table "ingredients", force: true do |t|
+    t.string   "name"
+    t.integer  "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "recipe_id"
+  end
+
+  add_index "ingredients", ["recipe_id"], name: "index_ingredients_on_recipe_id"
+
+  create_table "recipes", force: true do |t|
+    t.string   "name"
+    t.time     "preparation_time"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  add_index "recipes", ["user_id"], name: "index_recipes_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
